e6ca0b99798ed5452ec4869784d23d6adc3970f5
Alvin gomez <alvingomez.world@gmail.com>
2021-05-13 00:01:51 +0800

    Sidemenu refactoring
    Refactored the sideMenu code from index.js to  sideMenu component

---
+++ components/sideMenu.js
@@ -0,0 +1,14 @@
+const sideMenu = () => {
+    return(
+    <>
+        <h1 className="my-4">Shop Name</h1>
+        <section className="list-group">
+            <a href="#" className="list-group-item">Category 1</a>
+            <a href="#" className="list-group-item">Category 2</a>
+            <a href="#" className="list-group-item">Category 3</a>
+        </section>
+    </>
+    )
+}
+
+export {sideMenu as default}
