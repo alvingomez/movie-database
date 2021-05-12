e3821cae117cdf05b72acb9845705428b233e804
Alvin gomez <alvingomez.world@gmail.com>
2021-05-13 00:09:37 +0800

    Carousel refactoring
    Refactored the carousel code from index.js to  carousel component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'; 
 import NavBar from '../components/navbar';
 import SideMenu from '../components/sideMenu';
+import Carousel from '../components/carousel';
 
 const Home = () => (
   <div>
@@ -14,7 +15,7 @@ const Home = () => (
 
     <NavBar /> 
 
-  <div className="container">
+    <div className="container">
     <div className="row">
 
       <div className="col-lg-3">
@@ -24,34 +25,10 @@ const Home = () => (
       </div>
 
       <div className="col-lg-9">
-        <div id="carouselExampleIndicators" className="carousel slide my-4" data-ride="carousel">
-          <ol className="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-          </ol>
-          <div className="carousel-inner" role="listbox">
-            <div className="carousel-item active">
-              <img className="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide" />
-            </div>
-            <div className="carousel-item">
-              <img className="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide" />
-            </div>
-            <div className="carousel-item">
-              <img className="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide" />
-            </div>
-          </div>
-          <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="sr-only">Previous</span>
-          </a>
-          <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="sr-only">Next</span>
-          </a>
-        </div>
+   
+      <Carousel />
 
-        <div className="row">
+       <div className="row">
 
           <div className="col-lg-4 col-md-6 mb-4">
             <div className="card h-100">
